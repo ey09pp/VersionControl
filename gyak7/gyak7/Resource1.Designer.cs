cb91bb81e2c999da1d6dfa169e9887bffa5aa03c
Barnabás Timár <barnabas.timar@stud.uni-corvinus.hu>
2019-12-01 11:44:33 +0100

    Utónévre commit

--- gyak7/gyak7/Resource1.Designer.cs
+++ gyak7/gyak7/Resource1.Designer.cs
@@ -70,7 +70,7 @@ namespace gyak7 {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Keresztnév.
+        ///   Looks up a localized string similar to Utónévre.
         /// </summary>
         internal static string FirstName {
             get {
